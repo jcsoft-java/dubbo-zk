7f3bc8ae01a9e3504781bf7e1552d4eaf07ebaf7
zjfwjm1987 <860193679@qq.com>
2020-01-12 12:47:18 +0800

    commit

---
+++ dubbo-consumer/src/test/java/com/jc/dubboconsumer/DubboConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jc.dubboconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DubboConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
