7f72b77d0bddf97693db9fef6a7af40f75f66479
zjfwjm1987 <860193679@qq.com>
2020-01-12 11:23:01 +0800

    first commit

---
+++ src/main/java/com/jc/dubbozk/DubboZkApplication.java
@@ -0,0 +1,13 @@
+package com.jc.dubbozk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DubboZkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DubboZkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jc/dubbozk/DubboZkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jc.dubbozk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DubboZkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
