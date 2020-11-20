9d03ffeb1a3cdd60f469feaa3e7289ec08df7a0d
zjfwjm1987 <860193679@qq.com>
2020-11-20 15:15:17 +0800

    测试

---
+++ dubbo-provider/src/main/java/com/jc/dubboprovider/service/impl/CostServiceImpl.java
@@ -0,0 +1,11 @@
+package com.jc.dubboprovider.service.impl;
+
+/*
+ *
+ * @Copyright: 2020JCSoft
+ * @author: Zjf
+ * @since: 2020/11/20 14:45
+ * @Version 1.0
+ */
+public class CostServiceImpl  {
+}
