1c2e87223a9f0642b734fe6b8821c5b437cfb8b4
zjfwjm1987 <860193679@qq.com>
2020-11-20 16:13:59 +0800

    修改完后

--- dubbo-provider/src/main/java/com/jc/dubboprovider/DubboProviderApplication.java
+++ dubbo-provider/src/main/java/com/jc/dubboprovider/DubboProviderApplication.java
@@ -15,7 +15,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DubboProviderApplication {
     public static void main(String[] args) {
-        SpringApplication.run(DubboProviderApplication.class, args);
+        SpringApplication.run(DubboProviderApplication.class,args);
     }
 
 }
--- dubbo-provider/src/main/java/com/jc/dubboprovider/service/impl/CostServiceImpl.java
+++ dubbo-provider/src/main/java/com/jc/dubboprovider/service/impl/CostServiceImpl.java
@@ -1,5 +1,9 @@
 package com.jc.dubboprovider.service.impl;
 
+import com.alibaba.dubbo.config.annotation.Service;
+import com.jc.dubboapi.service.CostService;
+import lombok.extern.slf4j.Slf4j;
+
 /*
  *
  * @Copyright: 2020JCSoft
@@ -7,5 +11,21 @@ package com.jc.dubboprovider.service.impl;
  * @since: 2020/11/20 14:45
  * @Version 1.0
  */
-public class CostServiceImpl  {
+@Service
+public class CostServiceImpl implements CostService {
+    /**
+     * 假设之前总花费了100
+     */
+    private final Integer totalCost = 1000;
+
+    /**
+     * 之前总和 加上 最近一笔
+     *
+     * @param cost
+     * @return
+     */
+    @Override
+    public Integer add(int cost) {
+        return totalCost + cost;
+    }
 }
