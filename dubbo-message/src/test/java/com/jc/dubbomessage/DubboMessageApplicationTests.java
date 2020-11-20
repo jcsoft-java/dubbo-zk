76a7529022a5022fb30a8ac2383def08120c2786
zjfwjm1987 <860193679@qq.com>
2020-11-20 08:42:24 +0800

    测试

---
+++ dubbo-message/src/test/java/com/jc/dubbomessage/DubboMessageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jc.dubbomessage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DubboMessageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
